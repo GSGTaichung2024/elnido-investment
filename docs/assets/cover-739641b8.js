8ef5f07cf4a40c02a54357fdcb1ee8cc76a2e5bf
mafiapatch <g8071107@gmail.com>
2024-11-20 16:00:23 +0800

    [modify] update dists files

---
+++ docs/assets/cover-739641b8.js
@@ -0,0 +1,1 @@
+const o="/assets/img/piecelio/about_bottom.webp",s="/assets/img/piecelio/room/presidential-loft/01.webp",e="/assets/img/piecelio/room/executive-studio/cover.webp";export{o as _,s as a,e as b};
