850a25770bc0f8f35aa78b8b49114f0d428ef883
mafiapatch <g8071107@gmail.com>
2024-11-21 17:02:33 +0800

    [modify] about structure finish

---
+++ docs/assets/cover-a8ca3430.js
@@ -0,0 +1,1 @@
+const e="/assets/img/piecelio/room/presidential-loft/01.webp",o="/assets/img/piecelio/room/executive-studio/cover.webp";export{e as _,o as a};
--- src/views/About.vue
+++ src/views/About.vue
@@ -10,7 +10,7 @@ export default {
   components: {
     Header,
     Footer,
-    WhatOnElNido
+    WhatOnElNido,
   },
   setup() {
     const router = useRouter();
@@ -95,8 +95,6 @@ export default {
               <strong>改變生活，拯救生命</strong><br />
               住在海邊的生活，讓我們發現自己其實有能力影響身邊的環境。不再只是旁觀，我們可以主動去改變他人的生活，甚至拯救瀕臨危機的生命，讓生活更有意義！
             </p>
-
-
           </div>
         </div>
       </div>
@@ -124,7 +122,7 @@ export default {
                 <p>
                   這片美麗的土地不僅吸引了來自世界各地的遊客，也擁有巨大的潛在市場：每年周邊600萬的潛在遊客，讓這裡充滿無限商機。然而，定居後我發現了一個明顯的問題——<strong>旅館供不應求</strong>。朋友想來拜訪，卻常常訂不到房間，這顯示出當地住宿市場的供需嚴重失衡。
                 </p>
-                <br>
+                <br />
                 <p>
                   作為一個熱愛創業、對挑戰充滿熱情的人，我不僅看到了這個機會，也決定將它變成我的實踐目標。<strong
                     >既然訂不到房，那我們就自己來蓋！</strong
@@ -134,7 +132,7 @@ export default {
                   因此我打造了愛妮島Nacpan海灘上的第一棟獨棟Airbnb民宿，以「好玩的心態」去嘗試，卻用經營旅館的專業態度來管理。在2019年，我的努力得到了肯定，這棟民宿被《Culture
                   Trip》評選為<strong>「愛妮島最酷的Airbnb」</strong>。
                 </p>
-                <br>
+                <br />
                 <p>
                   選擇留在愛妮島，不僅是因為它的自然之美，更是因為它的無限潛力。這裡是一個讓人可以實現夢想的地方，一個能讓熱情與創意閃耀的舞台。在這片天堂，我找到了一個屬於自己的方向，並用實際行動為它注入更多可能性。
                 </p>
@@ -143,14 +141,13 @@ export default {
                 </p>
               </section>
             </div>
-
           </div>
         </div>
 
         <div class="col-lg-6">
           <div class="about-right">
             <div class="play-area p-0">
-              <img src="/assets/img/first-airbnb.jpeg">
+              <img src="/assets/img/first-airbnb.jpeg" />
             </div>
 
             <div class="icon-shape-1">
@@ -164,61 +161,102 @@ export default {
       </div>
     </div>
   </div>
+  <div class="course-area container pt-100 pb-70">
+    <div class="container-fluid m-0 p-0">
+      <div class="row justify-content-center">
+        <div class="col-lg-5 col-xxl-6">
+          <div class="course-img-1">
+            <img src="/assets/img/course/2.jpg" alt="Course Images" />
+          </div>
+          <div class="course-dots-2">
+            <img src="/assets/img/shape/dots-pink.png" alt="Dots Images" />
+          </div>
+        </div>
 
-  <!-- Management Team -->
-  <section class="trainers-area pt-100 pb-70">
-    <div class="container">
-      <div class="section-title text-center mb-50">
-        <h2>專業的管理團隊</h2>
-        <p>擁有豐富國際經驗的專業團隊，確保項目品質與營運效益</p>
+        <div class="col-lg-7 col-xxl-6">
+          <div class="course-item-2">
+            <div class="course-text">
+              <span>Sea Turtle Protect Mission</span>
+              <h2>Sylvia與海龜保育的意外使命</h2>
+              <p>
+                動物一直是我的熱愛。從小我就夢想成為動物志工，因為對動物的無比熱愛，我已經如素14年。當我定居愛妮島時，驚訝地發現當地人有吃海龜蛋的習慣。我的第一反應是：「我們拿錢把蛋買回來！」這份衝動的初心成為了改變的開端。花了整整兩年的時間，我們逐漸影響了當地人，成功改變了這樣的文化習慣。
+              </p>
+              <p>
+                海龜寶寶的自然存活率極低，僅約1/1000。我們請教了專家，從零開始，逐步建立了海龜孵育中心。這一路走來，我們不斷學習，不停向專家請教，只希望能為海龜寶寶建立一個最安全的家，提升牠們的孵育率。
+              </p>
+              <p>
+                八年間，我們已成功孵育並野放了<strong>48,663隻</strong>海龜寶寶。這份成果源於最初的初心，也因持續的努力而更加堅定。
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
 
-      <div class="row justify-content-center">
-        <div class="col-lg-3 col-sm-6">
-          <div class="trainer-card">
-            <img src="https://placehold.co/400" alt="El Nido Images" />
-            <div class="trainer-content">
-              <h3>Manager</h3>
-              <span>主管1</span>
+      <div class="course-another pt-70">
+        <div class="row justify-content-center">
+          <div class="col-lg-7 col-xxl-6">
+            <div class="course-item-2">
+              <div class="course-text">
+                <span>Challenge</span>
+                <h2>從默默守護到疫情的挑戰</h2>
+                <p>
+                  一開始，我們的海龜保育是默默進行的，並未對外開放。然而，Covid疫情的來襲讓我們的旅客與經費瞬間歸零。於是，我們發起了「海龜寶寶認養」活動，希望藉此延續保育工作。
+                </p>
+                <p>
+                  參與認養的朋友可以選擇單隻或整窩的海龜寶寶，並會收到認養證書。我們還提供整個孵育箱的認養與彩繪選項。透過這個活動，我們不僅得到了身邊朋友的支持，更收到來自世界各地陌生人的愛心。這份溫暖使得認養活動能夠持續至今。
+                </p>
+                <h3>教育與行動：為海洋未來努力</h3>
+                <p>
+                  除了海龜保育，我們也致力於社區教育。我們長期贊助附近的學校，並帶著孩子們一起淨灘，親近大自然。每年，我們還舉辦限定的海龜團活動，邀請參與者一起體驗海龜野放，並參與淨灘、海底垃圾清潔，以及海龜保育課程。
+                </p>
+                <p>
+                  我們相信，教育是改變的起點。透過孩子影響家庭，進而影響到來的旅客，讓更多人了解正確的海洋知識。我們希望這份愛與責任感，能夠傳遞到更遠的地方，為海洋與海龜的未來帶來更多希望。
+                </p>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div class="col-lg-3 col-sm-6">
-          <div class="trainer-card">
-            <img src="https://placehold.co/400" alt="El Nido Images" />
-            <div class="trainer-content">
-              <h3>Manager</h3>
-              <span>主管1</span>
+          <div class="col-lg-5 col-xxl-6">
+            <div class="course-img-2">
+              <img src="/assets/img/course/2.jpg" alt="Course Images" />
             </div>
-          </div>
-        </div>
-
-        <div class="col-lg-3 col-sm-6">
-          <div class="trainer-card">
-            <img src="https://placehold.co/400" alt="El Nido Images" />
-            <div class="trainer-content">
-              <h3>Henry Hsu</h3>
-              <span>開發總監</span>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-lg-3 col-sm-6">
-          <div class="trainer-card">
-            <img src="https://placehold.co/400" alt="El Nido Images" />
-            <div class="trainer-content">
-              <h3>Manager</h3>
-              <span>主管1</span>
+            <div class="course-dots-2">
+              <img src="/assets/img/shape/dots-pink.png" alt="Dots Images" />
             </div>
           </div>
         </div>
       </div>
     </div>
-  </section>
-  <!-- Management Team End -->
+    <div class="icon-shape-3">
+      <i class="flaticon-swimming"></i>
+    </div>
+    <!-- <div class="icon-shape-4">
+      <i class="flaticon-diver"></i>
+    </div> -->
+  </div>
 
-  <WhatOnElNido/>
+
+  <!-- Introduction Section -->
+  <div class="intro-area pt-100">
+    <div class="container">
+      <div class="section-title text-center mb-50">
+        <h2>我們的成就</h2>
+        <p>我們是一對在愛妮島創業的台灣情侶，擁有Nacpan海灘上最大的土地，致力於實現旅遊與永續發展的平衡。我們的酒店獲得Agoda高分評價（多家酒店評分達8.6至9.3），並成為日本Piece集團的海外合作品牌。我們的夢想不僅是為遊客打造難忘的旅程，更希望透過行動為這片美麗的土地帶來永續的保護與未來。</p>
+      </div>
+    </div>
+  </div>
+
+
+  <!-- Full Width Image -->
+  <div class="full-width-image pb-70">
+    <img
+      src="/assets/img/group-investment-with-bg.webp"
+      alt="Piece Lio Exterior"
+      class="w-100"
+    />
+  </div>
+  
+  <WhatOnElNido />
 
   <Footer />
 </template>
